9b11d2b85d31ceb9b7437468c8f26f4b2ce79a68
platinumkp3 <platinumkp3@gmail.com>
2012-10-24 17:40:58 +0530

    index page changes

--- Buzzzest/index.php
+++ Buzzzest/index.php
@@ -2,38 +2,51 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="./css/main.css" type="text/css"/>
 <title>Buzzzest</title>
 </head>
 
 <body>
-<div id="header">
-Become a Buzzzest today! <a href="signup.php" >Sign Up1</a> || <a href="signup2.php" >Sign Up2</a> || <a href="signup3.php" >Sign Up3</a>
+	<div id="header" style="border-bottom:1px solid #000;">
+		Become a Buzzzest today! <a href="signup.php" >Sign Up1</a> || 
+		<a href="signup2.php" >Sign Up2</a> || <a href="signup3.php" >Sign Up3</a>
+		<div style="float:right;margin-right:1%;">
+			Alreday a Buzzzest <a href="" >Sign In </a>
+		</div>
+	</div>
 
-<div style="float:right">Alreday a Buzzzest <a href="" >Sign In </a></div>
-</div>
+	<div style="display:block;float:left;margin-left:35%;margin-top:3%;">
+		<image src="./images/bee2.gif" width="150" height="" style="margin-left:8%;"/><br/>
+		<b style="font-size:45px;margin-left:8%;">Buzzzest</b><br/>
+		<b>Lets Buzz & Zest Your World.</b><br/>
+		<div id="index_search">
+			<input type="text" style="width:100%;"/>
+		</div>
+		
+	</div>
+	<div id="sign_in">
+		<form name="frmlogin" id="frmlogin">
+			<table width="234" height="155" align="right"  cellpadding="0" cellspacing="0">
+				<tr>
+					<td colspan="2">Username</td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="text"  name="username" id="username" /></td>
+				</tr>
+				<tr>
+					<td colspan="2">Password</td></tr>
+				<tr>
+					<td colspan="2"><input type="password" name="passwrd" id="passwrd" /></td>
+				</tr>
+				<tr>
+					<td ><input type="submit" name="SignIn"  id="SignIn" /></td><td><a href="" >Forget Password?</a></td>
+				</tr>
+				<tr>
 
-<hr />
-
-<form name="frmlogin" id="frmlogin" >
-<table width="234" height="155" align="right"  cellpadding="0" cellspacing="0">
-<tr>
-<td colspan="2">Username</td>
-</tr>
-<tr>
-<td colspan="2"><input type="text"  name="username" id="username" /></td>
-</tr>
-<tr>
-<td colspan="2">Password</td></tr>
-<tr>
-<td colspan="2"><input type="password" name="passwrd" id="passwrd" /></td>
-</tr>
-<tr>
-<td ><input type="submit" name="SignIn"  id="SignIn" /></td><td><a href="" >Forget Password?</a></td>
-</tr>
-<tr>
-
-</tr>
-</table>
-</form>
+				</tr>
+			</table>
+		</form>
+	</div>
+	
 </body>
 </html>
